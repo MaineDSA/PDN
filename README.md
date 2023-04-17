076498c6261ff56eb06f0c35fc3252ed0d1bcec8
Wil T <wil.t.dsa@pm.me>
2023-04-17 19:49:19 +0000

    documentation and structure improvements

--- README.md
+++ README.md
@@ -3,12 +3,15 @@ Sends a community safety alert to subscribers.
 
 ## Usage
 ```
-usage: emergency-alert.py [-m MESSAGE]
+usage: emergency-alert.py [-t] [-m MESSAGE]
 
 optional arguments:
   -m, --message MESSAGE
+  -t, --test
 ```
-If MESSAGE is not supplied, test message will be sent to AN_TEST_PERSON.
+If MESSAGE is not supplied, user will be prompted for message entry.
+If not supplied via command line or prompt, test message will be sent to AN_TEST_PERSON.
+If AN_TEST_PERSON is not set, user will be prompted for entry of AN API ID for test person.
 
 All messages will be appended with opt-out notification, for compliance.
 
@@ -16,6 +19,6 @@ All messages will be appended with opt-out notification, for compliance.
 - TWILIO_ACCOUNT_SID: Twilio API key/ID
 - TWILIO_AUTH_TOKEN: Twilio API authentication token
 - TWILIO_FROM_PHONE: Phone number in format 12079111973
-- AN_TAG_ID: ID of Action Network tag containing links to people
+- AN_TAG_ID: ID of Action Network tag containing links to people (will prompt if not found)
 - AN_API_TOKEN: API Token for Action Network access
-- AN_TEST_PERSON: ID of Action Network person to send test message to
+- AN_TEST_PERSON: ID of Action Network person to send test message to (will prompt if not found)
--- emergency-alert.py
+++ emergency-alert.py
@@ -1,44 +1,100 @@
 import os
-from twilio.rest import Client # Use API credentials via environmental variables
+from twilio.rest import Client
+
+# Set up Twilio client with account SID and auth token from environment variables
 client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
 
 import argparse
+
+# Set up argument parser for command-line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--message", type=str) # Get message from -m/--message args or use default
-argmessage = parser.parse_args().message
+parser.add_argument("-m", "--message", type=str)
+parser.add_argument('-t', '--test')
+
+# Parse command-line arguments to check for test mode
+testmode = parser.parse_args().test
+#testmode = 1
 
 from parsons import ActionNetwork
-an = ActionNetwork() # Use API credentials via environmental variables
 
-def get_phone(person): # Find first mobile number for supplied person
+# Set up Action Network client
+an = ActionNetwork()
+
+def get_phone(person):
+    # Iterate through phone numbers and return the mobile number
     for phone in person['phone_numbers']:
-        if phone['number_type'] == str('Mobile') and str('number') in phone :
+        if phone['number_type'] == str('Mobile') and str('number') in phone:
             return phone['number']
 
-def create_message(person, message): # Set up message for supplied person. $NAME will be replaced with given_name.
+def send_message(person, message):
+    # Extract the given name from the person object
     given_name = person['given_name'] or ''
+    
+    # Replace the $NAME placeholder with the person's given name
     messagetosend = message.replace('$NAME', given_name)
+
+    # Get the person's phone number
     phone = get_phone(person)
+
+    if not messagetosend or messagetosend == "": return
+
+    # Send the message using the Twilio client
     client.messages.create(
-            body = messagetosend,
-            from_ = str('+') + os.environ['TWILIO_FROM_PHONE'],
-            to = str('+') + phone
-        )
+        body=messagetosend + str(" To opt out, reply \"STOP\"."),
+        from_=str('+') + os.environ['TWILIO_FROM_PHONE'],
+        to=str('+') + phone
+    )
 
-def get_person_from_tag(tagging): # Find all people linked in supplied tagging
-    if tagging['item_type'] == str('osdi:person'):
-        link = str(tagging['_links']['osdi:person']['href'])
-        id = link.replace('https://actionnetwork.org/api/v2/people/', '')
-        return an.get_person(id)
+def get_tagged_people(tag):
+    # Initialize an empty dictionary to store people
+    people = []
 
-def contact_tagged_people(tag): # Send message to each person in supplied tag
+    # Iterate through taggings and add people with the specified tag to the dictionary
     for tagging in tag['_embedded']['osdi:taggings']:
-        person = get_person_from_tag(tagging)
-        create_message(person, argmessage)
+        if tagging['item_type'] == str('osdi:person'):
+            link = str(tagging['_links']['osdi:person']['href'])
+            id = link.replace('https://actionnetwork.org/api/v2/people/', '')
+            people.append(an.get_person(id))
 
-if argmessage:
-    argmessage += str(" To opt out, reply \"STOP\".")
-    an_tag = an.get_tag(os.environ['AN_TAG_ID'] + str('/taggings'))
-    contact_tagged_people(an_tag)
-else:
-    create_message(an.get_person(os.environ['AN_TEST_PERSON']), str("Hi $NAME, this is a test of our emergency alert system. To opt out, reply \"STOP\"."))
+    return people
+
+def get_message():
+    # Return a test message if in test mode
+    if testmode:
+        return str("Hi $NAME, this is a test of our emergency alert system.")
+
+    # Get message from command-line arguments or user input
+    message = parser.parse_args().message
+    if not message:
+        message = input("Write a message. $NAME will be replaced with the person's first name. "
+                + "Be sure to identify the source of the message. Opt out text will be added automatically to the end. "
+                + "To send a test message, don't enter anything. Ctrl+C to cancel. "
+            )
+
+    return message
+
+def get_people():
+    # Get test person if in test mode
+    if testmode:
+        testperson_id = os.environ['AN_TEST_PERSON']
+        if not testperson_id:
+            testperson_id = input("What is the API ID of the Action Network Person to be sent the test message? ")
+
+        return [an.get_person(testperson_id)]
+
+    # Get the tag ID from environment variables or user input
+    tagvar = os.environ['AN_TAG_ID']
+    if not tagvar:
+        tagvar = input("What is the API ID of the Action Network Tag? ")
+
+    # Get the tag object from Action Network
+    an_tag = an.get_tag(tagvar + str('/taggings'))
+
+    # Return people with the specified tag
+    return get_tagged_people(an_tag)
+
+# Get the message and send it to each person
+message = get_message()
+people = get_people()
+for person in people:
+    send_message(person, message)
